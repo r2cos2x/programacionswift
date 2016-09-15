6cda78827faddac2d9be94278b345aeefec49cca
Marifer De Ajuria <mariadeajuria@gmail.com>
2016-09-15 01:15:02 -0500

    Initial Commit

---
+++ miniretoj1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  miniretoj1
+//
+//  Created by FERNANDA DE AJURIA P1 on 15/09/16.
+//  Copyright © 2016 FERNANDA DE AJURIA P1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
